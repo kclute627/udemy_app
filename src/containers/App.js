1c92e533064afa7f1911676c4728a35e88ee528a
Kyle C <kyclutter@gmail.com>
2018-11-25 20:08:10 -0600

    remove Error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -59,12 +59,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}><Person
+            return  <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
+              key={person.id}
               
-              changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
               
           })}
         </div>
